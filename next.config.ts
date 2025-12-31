517afc2b6db476be6676198fab451665fe04d451
pasitgut <pasitgut.dev@gmail.com>
2025-12-31 12:52:22 +0700

    feat: new style website

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos'],
-  }
+    domains: ["picsum.photos"],
+    remotePatterns: [
+      new URL("https://avatars.githubusercontent.com/u/179317890?v=4"),
+    ],
+  },
 };
 
 export default nextConfig;
